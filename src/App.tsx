4b70530413748f5be0bca5571a919df8bf6f1555
vaibhav9852 <vaibhav.s@webkorps.com>
2024-11-22 01:08:18 +0530

    create react app with jest test

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Users from './Users/Users'
+
+function App() {  
+  return (
+    <>
+     {/* <h1>MERN DEMO</h1>   */}
+     <Users />
+    </>
+  ) 
+}
+
+export default App
+  
